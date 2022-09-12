5e5c81ef6cbb20a4b54942567e17926c11db7e5b
anclarma <anclarma@student.42.fr>
2022-09-12 13:54:43 +0200

    reverse proxy fonctionnel

--- srcs/frontend/src/app/services/api.service.ts
+++ srcs/frontend/src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Tech } from '../models/technology';
   providedIn: 'root'
 })
 export class ApiService {
-  API_SERVER = "http://localhost:3000/api";
+  API_SERVER = "https://localhost:8081/api";
 
   constructor(private httpClient: HttpClient) { }
 
