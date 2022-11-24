babb2b4d136b37f875550c4f7eff986117aadfa5
alex <alex2temmerman@gmail.com>
2022-11-24 15:17:15 +0100

    areyouready

--- srcs/frontend/src/app/widgets/are-you-ready/are-you-ready.component.ts
+++ srcs/frontend/src/app/widgets/are-you-ready/are-you-ready.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SocketService } from "../../services/socket.service";
+import { StorageService } from 'src/app/services/storage.service';
+import { User } from 'src/app/models/oauth';
+
 
 @Component({
   selector: 'app-are-you-ready',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./are-you-ready.component.css']
 })
 export class AreYouReadyComponent {
+
+  @Input() player1 !: User;
+  @Input() player2 !: User;
+
+  constructor(private socketService: SocketService, private storageService: StorageService){}
+
+  ngOnInit() {
+  }
+
+  imReady()
+  {
+    this.socketService.readySignal(this.player1.login, this.player2.login);
+  }
 
 }
