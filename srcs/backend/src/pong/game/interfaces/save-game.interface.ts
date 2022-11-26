6db9ffdb1349d6711264259f331d8aa05792e526
anclarma <anclarma@student.42.fr>
2022-11-26 17:48:51 +0100

    ajout des joueurs dans la fonction qui cree la game en back

--- srcs/backend/src/pong/game/interfaces/save-game.interface.ts
+++ srcs/backend/src/pong/game/interfaces/save-game.interface.ts
@@ -1,9 +1,9 @@
 import { User } from '@prisma/client';
 
 export interface SGame {
-	roomName:		string
-	player1: 		User
-	player2: 		User
-	player1_score:	number
-	player2_score:	number
-  }
+  roomName: string;
+  player1: User;
+  player2: User;
+  player1_score: number;
+  player2_score: number;
+}
