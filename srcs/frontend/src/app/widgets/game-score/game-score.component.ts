60eb3d6378010b448e1b06f6b8a60de187c1f390
Ching-lin LEE <chilee@student.42.fr>
2022-11-28 20:46:07 +0100

    updateGamescore2

--- srcs/frontend/src/app/widgets/game-score/game-score.component.ts
+++ srcs/frontend/src/app/widgets/game-score/game-score.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { User } from 'src/app/models/oauth';
 import { SGame } from 'src/app/models/savedGame';
 
 @Component({
@@ -14,17 +15,32 @@ export class GameScoreComponent {
   @Input() GameData!: SGame;
   @Output() closeScoreEvent = new EventEmitter<boolean>();
   Winner!:string;
-  // scorePlayer1!: number;
-  // scorePlayer2!: number;
+  scorePlayer1!: number;
+  scorePlayer2!: number;
+  winner!:User;
   
+
+  ngOnInit(): void {
+      this.winnerIs();
+  }
+
 
   closeGameScore(){
       this.closeScoreEvent.emit(false);
     }
 
+  winnerIs(){
+      this.scorePlayer1=this.GameData.player1_score;
+      this.scorePlayer2=this.GameData.player2_score;
+      if(this.scorePlayer1>this.scorePlayer2){
+        this.winner=this.GameData.player1;        
+      }
+      else{
+        this.winner=this.GameData.player2;
+      }
 
-
-
+  }
+    
 }
 
 
